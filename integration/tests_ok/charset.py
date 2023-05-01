6e69816df1f1603c19c47a3f5f0d294ecec014ab
Daniel Berecz <daniel_berecz@fastmail.com>
2023-05-01 13:09:30 +0200

    added integration test
    Signed-off-by: Daniel Berecz <daniel_berecz@fastmail.com>

--- integration/tests_ok/charset.py
+++ integration/tests_ok/charset.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import request, make_response
+from flask import Response, make_response
 
 
 @app.route("/charset/default")
@@ -12,3 +12,11 @@ def charset_uppercase():
     resp = make_response("<p>Hello World!</p>")
     resp.headers["Content-Type"] = "text/html; charset=UTF-8"
     return resp
+
+
+@app.route("/charset/api-version")
+def charset_api_version():
+    return Response(
+        """{ "version": "7.0-preview.1"}""",
+        mimetype="application/json; charset=UTF-8; api-version=7.0-preview.1",
+    )
